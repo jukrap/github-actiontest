47e4aa4569d2923aecccdcb12e6c6da00369b078
jukrap <jukrap628@gmail.com>
2024-05-27 21:36:54 +0900

    10

--- .github/workflows/generate_devcommentary.py
+++ .github/workflows/generate_devcommentary.py
@@ -1,4 +1,3 @@
-
 import os
 import pandas as pd
 
@@ -12,6 +11,9 @@ for file in csv_files:
 # 데이터프레임 합치기
 df = pd.concat(df_list, ignore_index=True)
 
+# 주차별로 내림차순 정렬
+df = df.sort_values('week', ascending=False)
+
 # 주차별로 그룹화
 weeks = df['week'].unique()
 
@@ -19,32 +21,32 @@ weeks = df['week'].unique()
 with open('_DevCommentary/README.md', 'w') as f:
     for week in weeks:
         f.write(f'## {week} 해설글\n')
-        
+
         # 플랫폼별로 그룹화
         platforms = df[df['week'] == week]['platform'].unique()
-        
+
         for platform in platforms:
             f.write(f'<details>\n<summary>{platform}</summary>\n\n')
-            
+
             # 문제별 해설글을 테이블로 정리
             f.write('| 문제 | 해설글 |\n')
             f.write('| --- | --- |\n')
-            
+
             # 문제별로 그룹화
             problems = df[(df['week'] == week) & (df['platform'] == platform)]['problem'].unique()
-            
+
             for problem in problems:
                 # 해당 문제에 대한 해설글 작성자와 URL
                 problem_df = df[(df['week'] == week) & (df['platform'] == platform) & (df['problem'] == problem)]
-                
+
                 f.write(f'| {problem} | ')
-                
+
                 for i, (_, row) in enumerate(problem_df.iterrows()):
                     f.write(f"[{row['name']}의 해설글]({row['url']})")
-                    
+
                     if i < len(problem_df) - 1:
                         f.write('<br>')
-                
+
                 f.write(' |\n')
-            
+
             f.write('\n</details>\n\n')
